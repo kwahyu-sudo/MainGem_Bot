94aa3adc4dfe66c4926b25d90433ce20275dcea3
kwahyu-sudo <kurniawan.wahyu.illahi@gmail.com>
2026-05-20 13:27:24 +0700

    fix import syntax

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import os
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 from groq import Groq
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 
